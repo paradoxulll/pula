6d657a764f048004b0b0d64875c761dea03067d3
paradoxulll <alexandruionutsava@gmail.com>
2025-10-05 03:25:54 +0300

    discord.js

--- config/discord.js
+++ config/discord.js
@@ -2,7 +2,7 @@ module.exports = {
   // Discord OAuth2 configuration
   clientId: process.env.DISCORD_CLIENT_ID || '1423655964090962084',
   clientSecret: process.env.DISCORD_CLIENT_SECRET || 'yn2lNQjpPybOMUOuEJWsASiGXp71zTs4',
-  redirectUri: process.env.DISCORD_REDIRECT_URI || 'https://discord.com/oauth2/authorize?client_id=1423655964090962084',
+  redirectUri: process.env.DISCORD_REDIRECT_URI || 'https://switrp.netlify.app/callback',
   
   // Discord API endpoints
   authUrl: 'https://discord.com/api/oauth2/authorize',
@@ -13,3 +13,4 @@ module.exports = {
   scopes: ['identify', 'email']
 
 };
+
